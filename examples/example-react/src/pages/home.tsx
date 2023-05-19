2d23f749d361b7967d9d3e2082eb6ca5caf5d1c4
julian-saleix <julian.saleix@gmail.com>
2023-05-19 18:24:51 +0200

    Adding mousemove listener in AnalyticContext + updating react example

--- examples/example-react/src/pages/home.tsx
+++ examples/example-react/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Button from "../components/button";
 import Form from "../components/form";
 
--- packages/ratflow-sdk-react/src/lib/context.tsx
+++ packages/ratflow-sdk-react/src/lib/context.tsx
@@ -5,6 +5,7 @@ import {
 } from "./interfaces/analytics-context";
 import React, { createContext, useContext, useEffect } from "react";
 import { sendEvent } from "ratflow-sdk-lib-front";
+import { debounce } from "./helpers";
 
 const AnalyticsContext = createContext<AnalyticsContextType>({
     fetchEvent: () => Promise.resolve(),
@@ -16,7 +17,7 @@ export const useAnalytics = () => {
 
     if (!context) {
         throw new Error(
-            "useAnalytics must be used within AnalyticsContextProvider"
+            "useAnalytics must be used within an AnalyticsContextProvider"
         );
     }
 
@@ -25,30 +26,48 @@ export const useAnalytics = () => {
 
 export const AnalyticsContextProvider: React.FC<
     AnalyticsContextProviderProps
-> = ({ children, config }: AnalyticsContextProviderProps) => {
-    const { token } = config;
+> = ({ children, auth, options }: AnalyticsContextProviderProps) => {
     const [currentPage, setCurrentPage] = React.useState<string | null>(null);
 
     const fetchEvent = async (event: FetchEventParams) => {
         const { tag, type, data } = event;
         const page = currentPage ? currentPage : window.location.pathname;
 
-        const authOptions = {
-            appId: token
-        }
+        if (!auth) return console.error("No auth provided");
 
         const dataOptions = {
-            tag: tag??"no",
+            tag: tag ?? "unset",
             eventName: type,
             url: page,
-            date: new Date()
-        }
+            date: new Date(),
+        };
 
-        const options = { 
+        const options = {
             useBeacon: true,
-        }
-        const res = await sendEvent({auth: authOptions, data: dataOptions, options});
+        };
+
+        await sendEvent({
+            auth,
+            data: dataOptions,
+            options,
+        });
     };
+
+    useEffect(() => {
+        if (!options?.trackMouse) return;
+
+        const listenerCb = debounce((e: MouseEvent) => {
+            const { pageX, pageY } = e;
+            fetchEvent({
+                type: "mousemove",
+                data: { pageX, pageY },
+            });
+        }, 1000);
+
+        document.addEventListener("mousemove", listenerCb);
+
+        return () => document.removeEventListener("mousemove", listenerCb);
+    }, [options]);
 
     useEffect(() => {
         if (!currentPage) return;
