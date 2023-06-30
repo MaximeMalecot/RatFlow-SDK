ea3e95be280b3f56c6620eae6fbf9d73684f1a37
FranciscoSacha <francisco.leblanc.sacha@gmail.com>
2023-06-30 15:50:41 +0200

    update interceptor in ratflflow-sdk-nest-ms

--- packages/ratflow-sdk-nest-ms/src/ratflow.interceptor.ts
+++ packages/ratflow-sdk-nest-ms/src/ratflow.interceptor.ts
@@ -1,20 +1,24 @@
-import {
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler,
-  } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { Observable } from "rxjs";
 import { sendEvent } from "ratflow-sdk-lib-back";
-  
-interface RatflowConfig {
-    appId: string;
-    appSecret: string;
-    service?: string;
-    options: {
-        showLogs?: boolean;
-    }
+
+interface RatflowAuth {
+  appId: string;
+  appSecret: string;
+  service?: string;
 }
-  
+
+interface RatflowOptions {
+  showLogs?: boolean;
+}
+
+interface RatflowConfig {
+  appId: string;
+  appSecret: string;
+  service?: string;
+  options: RatflowOptions;
+}
+
 interface RatflowData {
   eventName: string;
   url: string;
@@ -24,39 +28,47 @@ interface RatflowData {
   date: Date;
   customData?: any;
 }
-  
+
 export class RatFlowInterceptor implements NestInterceptor {
-    constructor(private config: RatflowConfig) {
-        this.config = config;
+  private auth: RatflowAuth;
+  private options: RatflowOptions;
+  constructor(config: RatflowConfig) {
+    const { options, ...auth } = config;
+    if (!auth.appId || !auth.appSecret) {
+      throw new Error("Missing SendEventAuth params");
     }
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        const ctx = context.switchToRpc();
-        const config = this.config;
-        const data = ctx.getData();
-        if(!data['url']) {
-          if(config.options.showLogs){
-              console.error("missing url");
-          }
-          ctx.getContext().getArgs()['sendRatflowIntercept'] = function () { };
-          return next.handle();
-        }
-        const reqData = {
-          userAgent: data['userAgent'],
-          url: data['url'],
-        };
-        ctx.getContext().getArgs()['sendRatflowIntercept'] = function ({
-          event,
-          ...rest
-        }) {
-          const data: RatflowData = {
-            ...reqData,
-            eventName: event,
-            date: new Date(),
-            customData: rest,
-          };
-          sendEvent({ auth: config, data });
-        };
-        return next.handle();
+    this.auth = auth;
+    this.options = options;
+  }
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToRpc();
+    const auth = this.auth;
+    const options = this.options;
+    const data = ctx.getData();
+    if (!data["url"]) {
+      if (options.showLogs) {
+        console.error("missing url");
+      }
+      ctx.getContext().getArgs()["sendRatflowIntercept"] = function () {};
+      return next.handle();
     }
+
+    const reqData = {
+      userAgent: data["userAgent"],
+      url: data["url"],
+    };
+    ctx.getContext().getArgs()["sendRatflowIntercept"] = function ({
+      event,
+      ...rest
+    }) {
+      const data: RatflowData = {
+        ...reqData,
+        eventName: event,
+        date: new Date(),
+        customData: rest,
+      };
+      sendEvent({ auth, data, options });
+    };
+    return next.handle();
+  }
 }
-  
